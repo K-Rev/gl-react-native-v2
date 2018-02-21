b6f1c208fbbe3f4c59ac9b8574c733cacb70e621
gmlion <gmlion@gmail.com>
2018-02-21 18:19:29 +0100

    update

--- example/index.android.js
+++
@@ -1,3 +0,0 @@
-import { AppRegistry } from "react-native";
-import example from "./src";
-AppRegistry.registerComponent("example", () => example);
--- example/index.ios.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry, StatusBar } from "react-native";
-import example from "./src";
-StatusBar.setHidden(true);
-AppRegistry.registerComponent("example", () => example);
